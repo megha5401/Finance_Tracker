d193e8ff279eaff2a668e148d93a7b84598bda29
megha5401 <165200185+megha5401@users.noreply.github.com>
2025-02-03 04:04:16 +0530

    Add files via upload

---
+++ src/components/DashboardPage.js
@@ -0,0 +1,10 @@
+// src/components/DashboardPage.js
+/*
+import React from 'react';
+
+const DashboardPage = () => {
+  return <h1>Welcome to your Dashboard</h1>;
+};
+
+export default DashboardPage;
+*/
---
+++ src/firebaseConfig.js
@@ -0,0 +1,25 @@
+/*// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDJFn9rBUAGEaUnGwaeN-heyqUNh9yEnr8",
+  authDomain: "finance-tracker-2a51b.firebaseapp.com",
+  projectId: "finance-tracker-2a51b",
+  storageBucket: "finance-tracker-2a51b.firebasestorage.app",
+  messagingSenderId: "649706667693",
+  appId: "1:649706667693:web:dc23ca6d24a4af51e51024",
+  measurementId: "G-47KCGZE8D4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const analytics = getAnalytics(app);
+export { auth }; // Export auth for use in your app
+*/
